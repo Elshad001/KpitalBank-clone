db56013ddb651baf57775380c23015253c647054
Elshad001 <elshad.ismayilovv@mail.ru>
2023-02-28 01:13:01 +0400

    add header

---
+++ script.js
@@ -0,0 +1,27 @@
+/*
+const dropdown=document.querySelector('.dropdown');
+const header2Titles=document.querySelectorAll('.header2-menu ul li');
+
+header2Titles.forEach((item)=>
+{
+    item.addEventListener('mouseover',()=>
+    {
+        dropdown.style.visibility='visible';
+    })
+
+    item.addEventListener('mouseout',()=>
+    {
+        dropdown.style.visibility='hidden';
+    })
+})
+
+dropdown.addEventListener('mouseover',()=>
+{
+    dropdown.style.visibility='visible'
+})
+
+dropdown.addEventListener('mouseout',()=>
+{
+    dropdown.style.visibility='hidden'
+})
+*/
