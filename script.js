0a7f89b517afb91a5eb04ce25fbb11b67dbc9c22
Elshad001 <elshad.ismayilovv@mail.ru>
2023-02-28 22:55:28 +0400

    add section4

--- script.js
+++ script.js
@@ -13,3 +13,36 @@ prev.addEventListener('click',()=>
 {
     section1.scrollLeft-=scrollAmount
 })
+
+const amountInput=document.querySelector('.amount-input');
+const amountRange=document.querySelector('.amount-range');
+amountInput.value=10000;
+amountInput.value=amountRange.value;
+amountRange.addEventListener('input',()=>
+{
+    amountInput.value=amountRange.value;
+})
+
+const percentInput=document.querySelector('.percent-input');
+const percentRange=document.querySelector('.percent-range');
+percentInput.value=15;
+percentInput.value=percentRange.value;
+percentRange.addEventListener('input',()=>
+{
+    percentInput.value=percentRange.value;
+})
+
+const timeInput=document.querySelector('.time-input');
+const timeRange=document.querySelector('.time-range');
+timeInput.value=3
+timeInput.value=timeRange.value;
+timeRange.addEventListener('input',()=>
+{
+    timeInput.value=timeRange.value;
+})
+
+const creditAmount=document.querySelector('.credit-amount')
+
+creditAmount.value=Math.round((parseInt(amountInput.value)+(parseInt(amountInput.value)*(parseInt(percentInput.value)/100)))/parseInt(timeInput.value));
+
+
